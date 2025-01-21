dcdffd6cb8315e60cafdae15e2bc43fc5187b475
Rasthian <rasthiandwiki@gmail.com>
2025-01-21 21:46:53 +0700

    feat:  add task feature

--- api-laravel-seal/app/Http/Controllers/TaskController.php
+++ api-laravel-seal/app/Http/Controllers/TaskController.php
@@ -2,9 +2,53 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Task;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    //
+    public function createTask(Request $request)
+    {
+        $status = '';
+        $message = '';
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'required|in:pending,in progress,completed', 
+            'project_id' => 'required|exists:projects,id_project', 
+            'user_id' => 'required|exists:users,id_user', 
+        ]);
+
+        try {
+            $newProject = Task::create([
+                'name' => $request->name,
+                'description' => $request->description,
+                'status' => $request->status,
+                'project_id' => $request->project_id,
+                'user_id' => $request->user_id,
+            ]);
+
+            if ($newProject) {
+                $message = 'Berhasil menambah data proyek';
+                $status_code = 201;
+            } else {
+                $message = 'Gagal menambah data proyek';
+                $status_code = 400;
+            }
+
+            $status = 'success';
+            $data = $newProject;
+        } catch (\Exception $e) {
+            $status = 'failed';
+            $message = 'Gagal menjalankan request: ' . $e->getMessage();
+            $status_code = 500;
+            $data = null;
+        } finally {
+            return response()->json([
+                'status' => $status,
+                'message' => $message,
+                'data' => $data
+            ], $status_code);
+        }
+    }
 }
